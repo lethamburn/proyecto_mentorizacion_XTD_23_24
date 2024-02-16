9a0c9053a675dc18e93a2c558a3467d8b2770ce8
Manuel Jimena García <Manuel.jimena29@gmail.com>
2024-02-16 11:50:37 +0100

    refact toggles

--- src/components/Toggle_MJG/Viewer.jsx
+++ src/components/Toggle_MJG/Viewer.jsx
@@ -3,10 +3,37 @@ import Toggle from './Toggle'
 import styled from 'styled-components'
 
 const ToggleViewerContainer = styled.div`
-  margin-top: 20px;
-  padding-left: 20px;
+  padding: 5px;
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 5px;
+`
+
+const MainTitle = styled.div`
+  font-family: 'Inter', sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 36px;
+  letter-spacing: 0px;
+  text-align: left;
+  color: var(--general100);
+  margin-bottom: 25px;
+`
+
+const ToggleSizeTitle = styled.div`
+  font-family: 'Inter', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0px;
+  text-align: left;
+  color: var(--general60);
+  margin-bottom: 10px;
+`
+
+const TogglesContainer = styled.div`
+  display: flex;
   gap: 20px;
 `
 
@@ -30,20 +57,24 @@ const ToggleViewer = () => {
 
   return (
     <ToggleViewerContainer>
-      <ToggleLabel>
-        <Toggle size="large" checked={checked.large} onChange={() => handleChange('large')} />
-        <span>Large</span>
-      </ToggleLabel>
+      <MainTitle>Toggle</MainTitle>
+      <ToggleSizeTitle>Toggle Size</ToggleSizeTitle>
+      <TogglesContainer>
+        <ToggleLabel>
+          <Toggle size="large" checked={checked.large} onChange={() => handleChange('large')} />
+          <span>Large</span>
+        </ToggleLabel>
 
-      <ToggleLabel>
-        <Toggle size="medium" checked={checked.medium} onChange={() => handleChange('medium')} />
-        <span>Medium</span>
-      </ToggleLabel>
+        <ToggleLabel>
+          <Toggle size="medium" checked={checked.medium} onChange={() => handleChange('medium')} />
+          <span>Medium</span>
+        </ToggleLabel>
 
-      <ToggleLabel>
-        <Toggle size="small" checked={checked.small} onChange={() => handleChange('small')} />
-        <span>Small</span>
-      </ToggleLabel>
+        <ToggleLabel>
+          <Toggle size="small" checked={checked.small} onChange={() => handleChange('small')} />
+          <span>Small</span>
+        </ToggleLabel>
+      </TogglesContainer>
     </ToggleViewerContainer>
   )
 }
