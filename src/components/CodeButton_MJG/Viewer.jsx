16fa5faaf13e43bd00629c71ab82c11f62394663
Manuel Jimena García <Manuel.jimena29@gmail.com>
2024-03-03 17:11:47 +0100

    refact CodeButton

--- src/components/CodeButton_MJG/Viewer.jsx
+++ src/components/CodeButton_MJG/Viewer.jsx
@@ -12,64 +12,64 @@ const Viewer = () => {
   }
 
   const codeString = `
-<style>
-.switch {
-  background-color: var(--general50);
-  width: 100px;
-  height: 20px;
-  border-radius: 5px;
-  padding: 1px;
-  display: inline-block;
-  position: relative;
-}
+    <style>
+      .switch {
+        background-color: var(--general50);
+        width: 100px;
+        height: 20px;
+        border-radius: 5px;
+        padding: 1px;
+        display: inline-block;
+        position: relative;
+      }
 
-.switch label {
-  float: left;
-  width: 50%;
-  line-height: 20px;
-  text-align: center;
-  cursor: pointer;
-  position: relative;
-  z-index: 2;
-  transition: color 0.3s ease;
-  text-transform: uppercase;
-  font-size: 10px;
-}
+      .switch label {
+        float: left;
+        width: 50%;
+        line-height: 20px;
+        text-align: center;
+        cursor: pointer;
+        position: relative;
+        z-index: 2;
+        transition: color 0.3s ease;
+        text-transform: uppercase;
+        font-size: 10px;
+      }
 
-.switch input[type="radio"] {
-  display: none;
-}
+      .switch input[type="radio"] {
+        display: none;
+      }
 
-.switch input[type="radio"]:checked + label {
-  font-weight: bold;
-}
+      .switch input[type="radio"]:checked + label {
+        font-weight: bold;
+      }
 
-.switch span {
-  position: absolute;
-  z-index: 1;
-  top: 1px;
-  left: 1px;
-  width: calc(50% - 2px);
-  height: calc(100% - 2px);
-  background-color: white;
-  border-radius: 4px;
-  transition: all 0.3s ease;
-}
+      .switch span {
+        position: absolute;
+        z-index: 1;
+        top: 1px;
+        left: 1px;
+        width: calc(50% - 2px);
+        height: calc(100% - 2px);
+        background-color: white;
+        border-radius: 4px;
+        transition: all 0.3s ease;
+      }
 
-.switch span.right {
-  left: calc(50% + 1px);
-}
-</style>
+      .switch span.right {
+        left: calc(50% + 1px);
+      }
+    </style>
 
-<div class="switch">
-  <input name="radio" type="radio" value="optionone" id="optionone">
-  <label for="optionone">Preview</label>
+    <div class="switch">
+      <input name="radio" type="radio" value="optionone" id="optionone">
+      <label for="optionone">Preview</label>
 
-  <input name="radio" type="radio" value="optiontwo" id="optiontwo" checked>
-  <label for="optiontwo" class="right">html</label>
+      <input name="radio" type="radio" value="optiontwo" id="optiontwo" checked>
+      <label for="optiontwo" class="right">html</label>
 
-  <span aria-hidden="true" class="right"></span>
-</div>
+      <span aria-hidden="true" class="right"></span>
+    </div>
   `.trim()
 
   const copyToClipboard = () => {
@@ -83,7 +83,7 @@ const Viewer = () => {
 
   return (
     <>
-      <CodeButton selected={selectedOption} onChange={handleChange} />
+      <CodeButton selected={selectedOption} onChange={handleChange} codeString={codeString} />
 
       {selectedOption === 'optiontwo' && (
         <div style={{ position: 'relative', borderRadius: '5px', overflow: 'hidden', border: '1px solid #ccc', marginTop: '10px' }}>
